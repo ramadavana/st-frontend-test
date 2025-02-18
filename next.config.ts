fe2b08e8d1aaa9997fe8e31be2628a9763af1ad7
ramadavana <davana1402@gmail.com>
2025-02-18 09:48:18 +0700

    next config fix

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  image: {
+  images: {
     domains: ["rickandmortyapi.com"],
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
